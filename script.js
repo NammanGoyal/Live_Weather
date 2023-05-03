0db2e1d94536bf752ceab370bbd15dbc3fca23f1
Namman Goyal <nammangoyal@gmail.com>
2023-05-03 23:39:04 +0530

    fix: typo

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ let weather = {
     },
     displayWeather:function(data){
         const { name } = data;
-        const { icon, discription } = data.weather[0];
+        const { icon, description } = data.weather[0];
         const { temp, humidity} = data.main;
         const{speed}= data.wind;
         document.querySelector(".city").innerText = "Weather in "+ name;
         document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+ icon +"@2x.png";
-        document.querySelector(".discription").innerText=discription;
+        document.querySelector(".description").innerText=description;
         document.querySelector(".temp").innerText= temp+ "°C";
         document.querySelector(".humidity").innerText="humidity: "+humidity+"%";
         document.querySelector(".wind").innerText="Wind Speed: "+speed+"km/h";
